170807017e864c73f802fb7b0f213f4dd11c90f5
Jon <due@wxwhatever.com>
2024-04-13 13:13:32 +1000

    Properly remap.

--- src/main/java/noobanidus/mods/mertradefix/mixins/MixinMermorphTipsProcedure.java
+++ src/main/java/noobanidus/mods/mertradefix/mixins/MixinMermorphTipsProcedure.java
@@ -10,17 +10,17 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(MermorphTipsProcedure.class)
 public class MixinMermorphTipsProcedure {
-  @Redirect(method = "execute(Lnet/minecraftforge/eventbus/api/Event;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/entity/Entity;)V", at=@At(value = "INVOKE", target = "Lnet/minecraft/network/chat/Component;literal(Ljava/lang/String;)Lnet/minecraft/network/chat/MutableComponent;", ordinal=0), remap=false)
+  @Redirect(method = "execute(Lnet/minecraftforge/eventbus/api/Event;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/entity/Entity;)V", at=@At(value = "INVOKE", target = "Lnet/minecraft/network/chat/Component;literal(Ljava/lang/String;)Lnet/minecraft/network/chat/MutableComponent;", ordinal=0))
   private static MutableComponent MerTradeFixFirstString(String p_237114_) {
     return Component.translatable("info.mermorph_trade_aquamarine");
   }
 
-  @Redirect(method = "execute(Lnet/minecraftforge/eventbus/api/Event;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/entity/Entity;)V", at=@At(value = "INVOKE", target = "Lnet/minecraft/network/chat/Component;literal(Ljava/lang/String;)Lnet/minecraft/network/chat/MutableComponent;", ordinal=1), remap=false)
+  @Redirect(method = "execute(Lnet/minecraftforge/eventbus/api/Event;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/entity/Entity;)V", at=@At(value = "INVOKE", target = "Lnet/minecraft/network/chat/Component;literal(Ljava/lang/String;)Lnet/minecraft/network/chat/MutableComponent;", ordinal=1))
   private static MutableComponent MerTradeFixSecondString(String p_237114_) {
     return Component.translatable("info.mermorph_trade_fish");
   }
 
-  @Redirect(method = "execute(Lnet/minecraftforge/eventbus/api/Event;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/entity/Entity;)V", at=@At(value = "INVOKE", target = "Lnet/minecraft/network/chat/Component;literal(Ljava/lang/String;)Lnet/minecraft/network/chat/MutableComponent;", ordinal=2), remap=false)
+  @Redirect(method = "execute(Lnet/minecraftforge/eventbus/api/Event;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/entity/Entity;)V", at=@At(value = "INVOKE", target = "Lnet/minecraft/network/chat/Component;literal(Ljava/lang/String;)Lnet/minecraft/network/chat/MutableComponent;", ordinal=2))
   private static MutableComponent MerTradeFixThirdString(String p_237114_) {
     return Component.translatable("info.mermorph_trade_nautilus");
   }
